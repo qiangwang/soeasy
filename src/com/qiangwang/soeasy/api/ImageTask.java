5c79b01f625d89e259ecd7a022ed178b1bf3fcff
xys <wsxys08@gmail.com>
2013-03-23 20:49:32 +0800

    add image cache and support lib

--- src/com/qiangwang/soeasy/api/ImageTask.java
+++ src/com/qiangwang/soeasy/api/ImageTask.java
@@ -14,46 +14,46 @@ import android.util.Log;
 
 public class ImageTask extends AsyncTask<Object, Void, Map<String, Object>> {
 
-    public static final String TAG = "ImageTask";
+	public static final String TAG = "ImageTask";
 
-    public static final int CONNECT_TIMEOUT = 5000;
-    public static final int READ_TIMEOUT = 10000;
+	public static final int CONNECT_TIMEOUT = 5000;
+	public static final int READ_TIMEOUT = 10000;
 
-    @Override
-    protected Map<String, Object> doInBackground(Object... params) {
-        Map<String, Object> resultMap = new HashMap<String, Object>();
+	@Override
+	protected Map<String, Object> doInBackground(Object... params) {
+		Map<String, Object> resultMap = new HashMap<String, Object>();
 
-        String url = (String) params[0];
-        APIListener<Bitmap, Exception> listener = (APIListener<Bitmap, Exception>) params[1];
+		String url = (String) params[0];
+		APIListener<Bitmap, Exception> listener = (APIListener<Bitmap, Exception>) params[1];
 
-        try {
-            Log.d(TAG, url);
+		try {
+			Log.d(TAG, url);
 
-            URLConnection conn = new URL(url).openConnection();
-            conn.setConnectTimeout(CONNECT_TIMEOUT);
-            conn.setReadTimeout(READ_TIMEOUT);
-            Bitmap bitmap = BitmapFactory.decodeStream((InputStream) conn
-                    .getContent());
+			URLConnection conn = new URL(url).openConnection();
+			conn.setConnectTimeout(CONNECT_TIMEOUT);
+			conn.setReadTimeout(READ_TIMEOUT);
+			Bitmap bitmap = BitmapFactory.decodeStream((InputStream) conn
+					.getContent());
 
-            resultMap.put("result", bitmap);
-        } catch (IOException e) {
-            resultMap.put("result", e);
-            Log.d(TAG, "doInBackground", e);
-        }
-        resultMap.put("listener", listener);
-        return resultMap;
-    }
+			resultMap.put("result", bitmap);
+		} catch (IOException e) {
+			resultMap.put("result", e);
+			Log.d(TAG, "doInBackground", e);
+		}
+		resultMap.put("listener", listener);
+		return resultMap;
+	}
 
-    @Override
-    protected void onPostExecute(Map<String, Object> resultMap) {
-        Object result = resultMap.get("result");
-        APIListener<Bitmap, Exception> listener = (APIListener<Bitmap, Exception>) resultMap
-                .get("listener");
-        if (result instanceof Bitmap) {
-            listener.onSuccess((Bitmap) result);
-        } else if (result instanceof Exception) {
-            listener.onError((Exception) result);
-        }
-    }
+	@Override
+	protected void onPostExecute(Map<String, Object> resultMap) {
+		Object result = resultMap.get("result");
+		APIListener<Bitmap, Exception> listener = (APIListener<Bitmap, Exception>) resultMap
+				.get("listener");
+		if (result instanceof Bitmap) {
+			listener.onSuccess((Bitmap) result);
+		} else if (result instanceof Exception) {
+			listener.onError((Exception) result);
+		}
+	}
 
 }
--- src/com/qiangwang/soeasy/api/ViewUtils.java
+++ src/com/qiangwang/soeasy/api/ViewUtils.java
@@ -3,6 +3,7 @@ package com.qiangwang.soeasy.api;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
 import android.text.TextUtils;
 import android.util.Log;
 import android.webkit.WebView;
@@ -10,34 +11,60 @@ import android.widget.ImageView;
 
 public class ViewUtils {
 
-    public static final String TAG = "ViewUtils";
+	public static final String TAG = "ViewUtils";
 
-    @SuppressLint("SetJavaScriptEnabled")
-    public static WebView getAuthWebView(Context context) {
-        WebView mWebView = new WebView(context);
-        mWebView.setVerticalScrollBarEnabled(false);
-        mWebView.setHorizontalScrollBarEnabled(false);
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        return mWebView;
-    }
+	private static LruCache<String, Bitmap> bitmapCache;
 
-    public static void setImage(final ImageView view, final String url) {
-    	if(TextUtils.isEmpty(url)) return;
-    	
-        new ImageTask().execute(url, new APIListener<Bitmap, Exception>() {
+	static {
+		final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
 
-            @Override
-            public void onSuccess(Bitmap result) {
-                view.setImageBitmap(result);
-            }
+	    // Use 1/8th of the available memory for this memory cache.
+	    final int cacheSize = maxMemory / 8;
+		
+		bitmapCache = new LruCache<String, Bitmap>(cacheSize) {
+			@Override
+			protected int sizeOf(String key, Bitmap bitmap) {
+				// The cache size will be measured in kilobytes rather than
+				// number of items.
+				return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
+			}
+		};
+	}
 
-            @Override
-            public void onError(Exception e) {
-                Log.e(TAG, "setImage:" + url, e);
-            }
+	@SuppressLint("SetJavaScriptEnabled")
+	public static WebView getAuthWebView(Context context) {
+		WebView mWebView = new WebView(context);
+		mWebView.setVerticalScrollBarEnabled(false);
+		mWebView.setHorizontalScrollBarEnabled(false);
+		mWebView.getSettings().setJavaScriptEnabled(true);
+		return mWebView;
+	}
 
-        });
+	public static void setImage(final ImageView view, final String url) {
+		if (TextUtils.isEmpty(url))
+			return;
 
-    }
+		Bitmap bitmap = bitmapCache.get(url);
+		if(bitmap != null){
+			view.setImageBitmap(bitmap);
+			return;
+		}
+		
+		new ImageTask().execute(url, new APIListener<Bitmap, Exception>() {
+
+			@Override
+			public void onSuccess(Bitmap result) {
+				view.setImageBitmap(result);
+				bitmapCache.put(url, result);
+			}
+
+			@Override
+			public void onError(Exception e) {
+				Log.e(TAG, "setImage:" + url, e);
+			}
+
+		});
+
+	}
 
 }
